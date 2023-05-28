5bdfc16f03d5c819e6555d252f9c017c9fe1fbb0
Hoang Do <hoangdo.2194@gmail.com>
2023-05-28 23:32:22 +0700

    Initial commit

---
+++ packages/charity/src/test/java/vn/edu/funix/charity/CharityApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.funix.charity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
