2c5476923e1b2f084f891e3e4c97b6e8311b3529
hoangdo <hoangdo@topcv.vn>
2023-05-30 09:25:09 +0700

    Ref: Adapt keycloak for prod

--- packages/keycloak/startup.sh
+++ packages/keycloak/startup.sh
@@ -2,7 +2,7 @@
 
 EnvMode=${KC_ENV:-dev}
 
-if [ EnvMode = 'production' ]; then
+if [ $EnvMode = 'production' ]; then
     /opt/keycloak/bin/kc.sh start --proxy edge
 else
     /opt/keycloak/bin/kc.sh start-dev
