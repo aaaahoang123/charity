d765d3e3cf6375957eee7249102a5e01dfa2d4c1
hoangdo <hoangdo@topcv.vn>
2023-05-31 14:08:09 +0700

    A custom theme for keycloak, then it can add phone number when register

--- packages/keycloak/startup.sh
+++ packages/keycloak/startup.sh
@@ -5,5 +5,5 @@ EnvMode=${KC_ENV:-dev}
 if [ $EnvMode = 'production' ]; then
     /opt/keycloak/bin/kc.sh start --proxy edge
 else
-    /opt/keycloak/bin/kc.sh start-dev
+    /opt/keycloak/bin/kc.sh start-dev --spi-theme-static-max-age=-1 --spi-theme-cache-themes=false --spi-theme-cache-templates=false
 fi
